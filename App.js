8c57216bc9f686229f32f0d5e3ce193d6a272e6b
Obiora Chukwuemeka Justice <83221540+ObioraCJ@users.noreply.github.com>
2022-06-02 16:19:12 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Counter from './React-projects/Counter';
+import SearchFilter from './React-projects/Counter';
+function App() {
+  return (
+    <div className='container'>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
